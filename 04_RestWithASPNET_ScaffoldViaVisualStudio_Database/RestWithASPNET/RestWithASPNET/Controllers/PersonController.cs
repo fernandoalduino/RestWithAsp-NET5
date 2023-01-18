92b683fe7c41359c950d020b4d01d25fdcfb7b0c
Fernando <krownf@protonmail.com>
2023-01-17 21:24:13 -0300

    Database actions

---
+++ 04_RestWithASPNET_ScaffoldViaVisualStudio_Database/RestWithASPNET/RestWithASPNET/Controllers/PersonController.cs
@@ -0,0 +1,77 @@
+using Microsoft.AspNetCore.Mvc;
+using RestWithASPNET.Models;
+using RestWithASPNET.Services;
+using System;
+
+namespace RestWithASPNET.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : ControllerBase
+    {
+
+        private readonly ILogger<PersonController> _logger;
+
+        private IPersonService _personService;
+
+        public PersonController(ILogger<PersonController> logger, IPersonService personService)
+        {
+            _logger = logger;
+            _personService = personService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_personService.GetAll());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(long id)
+        {
+            var person = _personService.FindById(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] Person person)
+        {
+            if (person == null)
+            {
+                return BadRequest("Person cannot be null");
+            }
+
+            return Ok(_personService.Create(person));
+        }
+
+        [HttpPut]
+        public IActionResult Update([FromBody] Person person)
+        {
+            if (person == null)
+            {
+                return BadRequest("Person cannot be null");
+            }
+
+            return Ok(_personService.Update(person));
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            var person = _personService.FindById(id);
+            if (person == null)
+            {
+                return BadRequest("Invalid Person");
+            }
+
+            _personService.Delete(id); // Void Method
+
+            return NoContent();
+        }
+    }
+}
